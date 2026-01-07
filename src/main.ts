9ef65515a693be756488f2ccef89a150e6d7cb0e
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-01-07 18:03:05 +0100

    feat: add swagger and logging to the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, VersioningType } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './modules/app/app.module';
+import { createSwaggerDocumentation } from './swagger';
+import { LoggingInterceptor } from './interceptors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Bootstrap');
+  const port = configService.get<number>('HTTP_PORT', 3001);
+  const globalPrefix = 'api';
+
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'v',
+  });
+
+  createSwaggerDocumentation(app, port, configService, globalPrefix);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://0.0.0.0:${port}`);
 }
+
 bootstrap();
